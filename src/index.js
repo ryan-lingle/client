9b4ca6f50582245cecb44ac02fe1751e3e36dbb2
Ryan Lingle <lingleryan@gmail.com>
2019-09-24 15:42:57 -0400

    test path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from '../src/app.js';
 import './style/index.scss'
+import App from './app.js';
 
 import { ApolloProvider } from 'react-apollo';
 
